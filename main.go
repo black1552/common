5d85bf306a99a4ec1e5f93df47eb3976bfc40bdb
black1552 <black1552@163.com>
2021-12-31 22:32:43 +0800

    添加一个创建TCP的http

--- main.go
+++ main.go
@@ -43,6 +43,28 @@ func StartCorn(upload, root string, time time.Duration, cronTime, name string, c
 	s.Run()
 }
 
+// StartTcp 创建一个TCP的http
+func StartTcp(upload, root string, time time.Duration, tcpFun func()) {
+	go func() {
+		s := g.Server()
+		s.SetServerRoot(root)
+		if !isDir(upload) {
+			_ = os.MkdirAll(upload, os.ModePerm)
+			s.AddStaticPath("/upload", upload)
+		}
+		s.SetFileServerEnabled(true)
+		_ = s.SetConfigWithMap(g.Map{
+			"SessionMaxAge":  time,
+			"SessionStorage": gsession.NewStorageMemory(),
+		})
+		s.Use(MiddlewareError)
+		s.Run()
+	}()
+
+	go tcpFun()
+	g.Wait()
+}
+
 func isDir(path string) bool {
 	s, err := os.Stat(path)
 	if err != nil {
